4a371013497f492383b09577098c30fc3aaa8c52
Ouzii <oskari.laaja@gmail.com>
2018-01-20 13:57:01 +0200

    Osa1 loppuun ja T1.6

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,34 +1,71 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class Hello extends React.Component {
-  render() {
-    const bornYear = () => {
-      const yearNow = 1900 + new Date().getYear()
-      return yearNow - this.props.age
-    }
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
+const Display = ({ counter }) => <div>{counter}</div>
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      vasen: 0,
+      oikea: 0,
+      kaikki: []
+    }
+  }
+
+  klikVasen = () => {
+    this.setState({
+      vasen: this.state.vasen + 1,
+      kaikki: this.state.kaikki.concat('v')
+    })
+  }
+
+  klikOikea = () => {
+    this.setState({
+      oikea: this.state.oikea + 1,
+      kaikki: this.state.kaikki.concat('o')
+    })
+  }
+
+  render() {
+    const historia = () => {
+      if (this.state.kaikki.length === 0) {
+        return (
+          <div>
+            <em>sovellusta käytetään nappeja painelemalla</em>
+          </div>
+        )
+      }
+      return (
+        <div>
+          näppäilyhistoria: {this.state.kaikki.join(' ')}
+        </div>
+      )
+    }
     return (
       <div>
-        <p>
-          Hello {this.props.name}, you are {this.props.age} years old <br />
-          So you were propably born {bornYear()}
-        </p>
+        <div>
+          {this.state.vasen}
+          <button onClick={this.klikVasen}>vasen</button>
+          <button onClick={this.klikOikea}>oikea</button>
+          {this.state.oikea}
+          <div>{historia()}</div>
+        </div>
       </div>
     )
   }
 }
 
-const App = () => {
-  const nimi = 'Pekka'
-  const ika = 10
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Arto" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
-    </div>
-  )
-}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ t1.6/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hyva: 0,
+            huono: 0,
+            neutraali: 0
+        }
+    }
+
+    klikHyva = () => {
+        this.setState({
+            hyva: this.state.hyva + 1
+        })
+    }
+
+    klikHuono = () => {
+        this.setState({
+            huono: this.state.huono + 1
+        })
+    }
+
+    klikNeutraali = () => {
+        this.setState({
+            neutraali: this.state.neutraali + 1
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Anna palautetta</h1>
+                <div>
+                    <button onClick={this.klikHyva}>Hyvä</button>
+                    <button onClick={this.klikNeutraali}>Neutraali</button>
+                    <button onClick={this.klikHuono}>Huono</button>
+                </div>
+                <h1>Statistiikka</h1>
+
+                <p> Hyvä: {this.state.hyva} </p>
+                <p> Neutraali: {this.state.neutraali} </p>
+                <p> Huono: {this.state.huono} </p>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
